ba39c98e52184bfff911993db87d564947293c3a
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-08-20 01:22:56 +0530

    Add files via upload

---
+++ C/Matrices/sparsematrix.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Element{
+  int i;
+  int j;
+  int x;
+};
+
+struct sparse{
+  int m;
+  int n;
+  int num;
+  struct Element *e;
+};
+typedef struct sparse sparse;
+void create(struct sparse *s){
+  int i;
+  printf("Enter Dimensions:\n");
+  scanf("%d %d",&s->m,&s->n);
+  printf("Enter no. of non zero elements:\n ");
+  scanf("%d",&s->num);
+s->e=(struct Element *)malloc(sizeof(struct Element));
+printf("Enter all elements:\n");
+for(i=0;i<s->num;i++)
+scanf("%d%d%d",&s->e[i].i,&s->e[i].j,&s->e[i].x);
+}
+
+int  add(struct sparse *s1,struct sparse *s2){
+ sparse *sum;
+ int i,j,k;
+ i=j=k=0;
+  if(s1->m!=s2->m||s1->n!=s2->n)
+  return 0;
+ sum=(struct sparse *)malloc(sizeof(struct sparse));
+ sum->e=(struct Element *)malloc((s1->num+s2->num)*sizeof(struct Element));
+ while(i<s1->num && j<s2->num)
+    {        if(s1->e[i].i<s2->e[j].i)
+                  sum->e[k++]=s1->e[i++];
+            else if(s1->e[i].i>s2->e[j].i)
+            sum->e[k++]=s2->e[j++];
+      else        {
+if(s1->e[i].j<s2->e[j].j)
+                sum->e[k++]=s1->e[i++];
+  else if(s1->e[i].j>s2->e[j].j)
+    sum->e[k++]=s2->e[j++];
+    else
+    {     sum->e[k]=s1->e[i];
+    sum->e[k++].x=s1->e[i++].x+s2->e[j++].x;            }                    }    }
+
+
+    for(;i<s1->num;i++)sum->ele[k++]=s1->ele[i];
+       for(;j<s2->num;j++)sum->ele[k++]=s2->ele[j];
+        sum->m=s1->m;    sum->n=s1->n;    sum->num=k;
+               return sum;
+}
+
+void main(){
+ struct Sparse s1,s2,*s3;
+        create(&s1);    create(&s2);
+          s3=add(&s1,&s2);
+             printf("First Matrix\n");
+                display(s1);
+                  printf("Second Matrix\n");
+                    display(s2);
+                        printf("Sum Matrix\n");
+                 display(*s3);             }
